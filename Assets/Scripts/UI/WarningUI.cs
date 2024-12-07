e882b4743d806780d8b52e64e4fb9b044f0f8532
ZaferYilmaz-Engineer <200604024@bakircay.edu.tr>
2024-12-07 16:44:10 +0300

    Added background to warning UI

--- Assets/Scripts/UI/WarningUI.cs
+++ Assets/Scripts/UI/WarningUI.cs
@@ -5,13 +5,17 @@ using DG.Tweening;
 using TMPro;
 using UnityEngine;
 using UnityEngine.Serialization;
+using UnityEngine.UI;
 
 public class WarningUI : MonoBehaviour
 {
     public static WarningUI Instance { get; private set; }
     
     [SerializeField] private GameObject panel;
-    [SerializeField] private TextMeshProUGUI warningTransform;
+    [SerializeField] private TextMeshProUGUI warningText;
+    [SerializeField] private Image backgroundImage;
+
+    private float backgroundAlpha;
 
     private void Awake()
     {
@@ -26,29 +30,39 @@ public class WarningUI : MonoBehaviour
 
     private void Start()
     {
+        backgroundAlpha = backgroundImage.color.a;
         panel.SetActive(false);
     }
 
     public void ShowWarning()
     {
-        warningTransform.transform.localScale = Vector3.one;
+        warningText.transform.localScale = Vector3.one;
         
-        var newColor = warningTransform.color;
+        var newColor = warningText.color;
         newColor.a = 0;
-        warningTransform.color = newColor;
+        warningText.color = newColor;
+
+        var newBackgroundAlphaColor = backgroundImage.color;
+        newBackgroundAlphaColor.a = 0;
+        backgroundImage.color = newBackgroundAlphaColor;
         
         panel.SetActive(true);
 
-        warningTransform.DOKill();
-
+        warningText.DOKill();
+        backgroundImage.DOKill();
+        
         Sequence sequence = DOTween.Sequence();
-        Tween textFadeInTween = warningTransform.DOFade(1, 0.5f);
-        Tween textScaleTween = warningTransform.transform.DOScale(Vector3.one * 1.2f, 0.5f).SetLoops(3, LoopType.Yoyo);
-        Tween textFadeOutTween = warningTransform.DOFade(0, 0.5f).SetDelay(1);
+        Tween backgroundFadeInTween = backgroundImage.DOFade(backgroundAlpha, 0.5f);
+        Tween textFadeInTween = warningText.DOFade(1, 0.5f);
+        Tween textScaleTween = warningText.transform.DOScale(Vector3.one * 1.2f, 0.5f).SetLoops(3, LoopType.Yoyo);
+        Tween textFadeOutTween = warningText.DOFade(0, 0.5f).SetDelay(1);
+        Tween backgroundFadeOutTween = backgroundImage.DOFade(0, 0.5f);
 
-        sequence.Append(textFadeInTween);
+        sequence.Append(backgroundFadeInTween);
+        sequence.Join(textFadeInTween);
         sequence.Join(textScaleTween);
         sequence.Join(textFadeOutTween);
+        sequence.Join(backgroundFadeOutTween);
 
         sequence.onComplete += () =>
         {
